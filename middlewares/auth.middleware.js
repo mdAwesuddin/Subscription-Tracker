129f65fb01dcdb478174ace15ee72bbcb274069e
mdAwesuddin <mdawesuddin@gmail.com>
2025-02-11 23:17:56 +0530

    added controllers and middlewares

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
+import { JWT_SECRET } from "../config/env.js";
+
+const authorize = async (req, res, next) => {
+    try {
+        let token;
+        
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if(!token) return res.status(401).json({ message: 'Unauthorized'});
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId);
+
+        if(!user) return res.status(401).json({message: 'Unauthorized'});
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        res.status(401).json({message: 'Unauthorized', error: error.message });
+    }
+}
+
+// e.g: someone is making request get user details -> authorize niddleware -> verify -> if vaild -> next -> get user details
+
+export default authorize;
